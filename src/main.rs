a973adc24901101e633b22e84f57c8cf525f8138
James Hawley <jamesrichardhawley@gmail.com>
2020-05-17 20:54:09 -0400

    Abstracting line parsing to a separate function

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,61 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, Write};
 use clap::{App, Arg};
 
+fn parse_markdown_line(contents: &str, mut _ptag: bool, mut _htag: bool) -> (String, bool, bool) {
+    // parse first character from line
+    let mut first_char: Vec<char> = contents.chars().take(1).collect();
+    let mut outline = String::new();
+
+    // see if the first character is a header line marker (i.e. '#')
+    match first_char.pop() {
+        Some('#') => {
+            // close paragraph, if currently open
+            if _ptag {
+                _ptag = false;
+                outline.push_str("</p>\n");
+            }
+            // if currently header, this line should be a new header
+            if _htag {
+                _htag = false;
+                outline.push_str("</h1>\n");
+            }
+
+            // set _htag to true, write new header
+            _htag = true;
+            outline.push_str("\n<h1>");
+            outline.push_str(&contents[2..]);
+        },
+        _ => {
+            // close header, if currently open
+            if _htag {
+                _htag = false;
+                outline.push_str("</h1>\n");
+            }
+            // if currently paragraph, continue
+            if !_ptag {
+                _ptag = true;
+                outline.push_str("<p>");
+            }
+
+            // add paragraph contents
+            outline.push_str(&contents);
+        }
+    }
+
+    // close appropriate tags
+    if _ptag {
+        _ptag = false;
+        outline.push_str("</p>\n");
+    }
+    // if currently header, this line should be a new header
+    if _htag {
+        _htag = false;
+        outline.push_str("</h1>\n");
+    }
+
+    return (outline, _ptag, _htag);
+}
+
 fn parse_markdown_file(_file: &str, _outfile: &str) {
     // input file path
     let infile = Path::new(_file);
@@ -11,6 +66,7 @@ fn parse_markdown_file(_file: &str, _outfile: &str) {
 
     let mut _ptag: bool = false; // keep track of paragraph tags
     let mut _htag: bool = false; // keep track of heading tags
+    let mut _outline: String; // parsed line contents to be written
 
     let mut tokens: Vec<String> = Vec::new(); // vector to store all tokens
     let reader = BufReader::new(file); // read line-by-line
@@ -18,60 +74,11 @@ fn parse_markdown_file(_file: &str, _outfile: &str) {
     // iterate through lines
     for line in reader.lines() {
         let contents = line.unwrap();
-        // parse first character from line
-        let mut first_char: Vec<char> = contents.chars().take(1).collect();
-        let mut outline = String::new();
-
-        // see if the first character is a header line marker (i.e. '#')
-        match first_char.pop() {
-            Some('#') => {
-                // close paragraph, if currently open
-                if _ptag {
-                    _ptag = false;
-                    outline.push_str("</p>\n");
-                }
-                // if currently header, this line should be a new header
-                if _htag {
-                    _htag = false;
-                    outline.push_str("</h1>\n");
-                }
-
-                // set _htag to true, write new header
-                _htag = true;
-                outline.push_str("\n<h1>");
-                outline.push_str(&contents[2..]);
-            },
-            _ => {
-                // close header, if currently open
-                if _htag {
-                    _htag = false;
-                    outline.push_str("</h1>\n");
-                }
-                // if currently paragraph, continue
-                if !_ptag {
-                    _ptag = true;
-                    outline.push_str("<p>");
-                }
-
-                // add paragraph contents
-                outline.push_str(&contents);
-            }
-        }
-
-        // close appropriate tags
-        if _ptag {
-            _ptag = false;
-            outline.push_str("</p>\n");
-        }
-        // if currently header, this line should be a new header
-        if _htag {
-            _htag = false;
-            outline.push_str("</h1>\n");
-        }
-
+        // parse the line of text
+        let (mut _outline, mut _ptag, mut _htag) = parse_markdown_line(&contents, _ptag, _htag);
         // check for empty lines, push to tokens
-        if outline != "<p></p>\n" {
-            tokens.push(outline);
+        if _outline != "<p></p>\n" {
+            tokens.push(_outline);
         }
     }
 
